7d28d0d0fe4e4744256d2c1ab29648403a89eaa2
msienkow <msienkow@gmail.com>
2021-11-24 12:03:32 -0600

    Initial Test

---
+++ SaniTrend.py
@@ -0,0 +1,59 @@
+import SaniTrendCloud
+import time
+import os
+from datetime import datetime
+from pycomm3 import LogixDriver
+from pycomm3.exceptions import CommError
+    
+def main():
+
+    # Set up SaniTrend parameters, tags, cloud configurations, etc...
+    SaniTrend = SaniTrendCloud.Config(ConfigFile='../SaniTrendConfig.json')
+
+    # Setup PLC Communication Driver
+    PLC = LogixDriver(SaniTrend.PLCIPAddress)
+
+    PLCErrorCount = 0
+    runCode = True
+
+    while runCode:
+        try:
+            # Get Connection Status For EMS
+            SaniTrend.ConnectionStatus()
+
+            if PLC.connected:
+                # Read PLC tags
+                tagData = PLC.read(*SaniTrend.Tags)
+
+                print(f'Connection Status = {SaniTrend.isConnected}')
+ 
+                # Reset PLC error count if everything succeeded
+                PLCErrorCount = 0
+                
+            else:
+                PLC.open()
+
+            time.sleep(0.5)
+
+        except CommError:
+            PLCErrorCount += 1
+            print(f'Communication Error! Fail Count: {PLCErrorCount}')
+            SaniTrend.LogErrorToFile('PLC Comms Failed', CommError)
+            if PLCErrorCount < 6:
+                time.sleep(10)
+            else:
+                time.sleep(30)
+            PLC = LogixDriver(SaniTrend.PLCIPAddress)
+
+        except KeyboardInterrupt:
+            print("\n\nExiting Python and closing PLC connection...\n\n\n")
+            PLC.close()
+            runCode = False
+            
+        except:
+            print('Shutting Down...')
+            PLC.close()
+            runCode = False
+
+if __name__ == "__main__":
+    main()
--- SaniTrendCloud.py
+++ SaniTrendCloud.py
@@ -18,14 +18,17 @@ class Config:
         self.Tags = []
         self.ServerURL = ''
         self.SMINumber = ''
-        self._AppKey = ''
+        self.ConnectionStatusTime= 10
+        self.isConnected = False
+        self._ConnectionStatusRunning = False
+        self._LastStatusUpdate = 0
+        self._ConnectionStatusSession = requests.Session()
         self._OS = platform.system()
         self.LoadConfig(ConfigFile=ConfigFile)
         self._HttpHeaders = {
             'Connection': 'keep-alive',
             'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            'appKey': self._AppKey
+            'Content-Type': 'application/json'
         }
         
 
@@ -33,10 +36,9 @@ class Config:
     def LoadConfig(self, *, ConfigFile):
         with open(ConfigFile) as file:
             self._configData = json.load(file)
-            self.PLCIPAddress = self._configData['Config']['PLCIPAddress']
-            self.ServerURL = self._configData['Config']['ServerURL']
+            self.PLCIPAddress = self._configData['Config']['PLCIPAddress']            
             self.SMINumber = self._configData['Config']['SMINumber']
-            self._AppKey = self._configData['Config']['AppKey']
+            self.ServerURL = f'http://localhost:8000/Thingworx/{self.SMINumber}/'
             self.Tags = self._configData['Tags']
         return self._configData
 
@@ -48,32 +50,40 @@ class Config:
         else:
             return None
 
-    # Simpe function to get current time in milliseconds. Useful for time comparisons
+    # Simple function to get current time in milliseconds. Useful for time comparisons
     # ex.  startTime = ObjectName.GetTimeMS()
     #      endTime = ObjectName.GetTimeMS()
     #      totalTimeDifferenceInMilliseconds = (endTime - startTime)
     def GetTimeMS(self,):
         return round(time.time() * 1000)
 
-    # Run RESTApi POST service to get current server time seconds
-    # Using number value more accurate than just boolean on/off
-    def _CloudWatchdog(self,):
-        url = self.ServerURL + 'Things/Connection_Test/Services/ConnectionTest'
+
+    # Function to check for connection status to Thingworx platform based upon time interval.
+    def ConnectionStatus(self):
+        timerPreset = self.ConnectionStatusTime * 1000
+        if (((self.GetTimeMS() - self._LastStatusUpdate) >= timerPreset) and not self._ConnectionStatusRunning):
+            self._ConnectionStatusRunning = True
+            self._LastStatusUpdate = self.GetTimeMS()
+            threading.Thread(target=self._ConnectionStatus).start()
+
+    #Run REST request against EMS to see if it is connected to the Thingworx platform
+    def _ConnectionStatus(self,):
+        url = 'http://localhost:8000/Thingworx/Things/LocalEms/Properties/isConnected'
         try:
-            serviceResult = self._CloudWatchdogSession.post(url, headers=self._CloudWatchdogHeaders, timeout=5)
+            serviceResult = self._ConnectionStatusSession.get(url, headers=self._HttpHeaders, timeout=5)
             if serviceResult.status_code == 200:
-                self.CloudWatchdogValue = (serviceResult.json())['rows'][0]['result']
+                self.isConnected = (serviceResult.json())['rows'][0]['isConnected']
                 
             else:
-                self.LogErrorToFile('_CloudWatchdog', serviceResult)
-                self.CloudWatchdogValue = 0
+                self.LogErrorToFile('_ConnectionStatus', serviceResult)
+                self.isConnected = False
 
         except Exception as e:
-            self.CloudWatchdogValue = 0
-            self.LogErrorToFile('_CloudWatchdog', e)
+            self.isConnected = False
+            self.LogErrorToFile('_ConnectionStatus', e)
             
         # Release Bit so watchdog can run again
-        self._CloudWatchdogRunning = False
+        self._ConnectionStatusRunning = False
 
 
     def LogErrorToFile(self, name, error):
