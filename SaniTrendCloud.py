c3fcc81ff3a34c7b57ab8c37eeda771877aa31da
msienkow <msienkow@gmail.com>
2021-11-29 13:47:34 -0600

    test

--- SaniTrendCloud.py
+++ SaniTrendCloud.py
@@ -1,119 +1,112 @@
-import platform
-import json
-from pycomm3.exceptions import CommError
-import threading
+import SaniTrendCloud
 import time
-from datetime import datetime
-from requests.models import HTTPError
-import requests
 import os
-from ftplib import FTP
-import shutil
-
-# Overall Configuration Class to import that has 
-# auxillary functions necesaary for the cloud
-class Config:
-    def __init__(self, *, ConfigFile=''):
-        self.PLCIPAddress = ''
-        self.Tags = []
-        self.TagTable = []
-        self.ServerURL = ''
-        self.SMINumber = ''
-        self.ConnectionStatusTime= 10
-        self.isConnected = False
-        self._ConnectionStatusRunning = False
-        self._LastStatusUpdate = 0
-        self._ConnectionStatusSession = requests.Session()
-        self._OS = platform.system()
-        self.LoadConfig(ConfigFile=ConfigFile)
-        self._HttpHeaders = {
-            'Connection': 'keep-alive',
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        }
-        
-
-    # Read in configuration file and set values on object
-    def LoadConfig(self, *, ConfigFile):
-        with open(ConfigFile) as file:
-            self._configData = json.load(file)
-            self.PLCIPAddress = self._configData['Config']['PLCIPAddress']            
-            self.SMINumber = self._configData['Config']['SMINumber']
-            self.ServerURL = f'http://localhost:8000/Thingworx/{self.SMINumber}/'
-            # self.Tags = self._configData['Tags']
-            self.TagTable = self._configData['Tags']
-            for dict in self.TagTable:
-                for x,y in dict.items():
-                    if x == 'tag':
-                        self.Tags.append(y)
-
-        return self._configData
-
-    # Get specific tag value from globally returned tag list from PLC through pycomm3
-    def GetTagValue(self, *, TagData=[], TagName=''):
-        if TagData and TagName:
-            result = [item.value for item in TagData if item[0] == TagName]
-            return result[0]
-        else:
-            return None
-
-    # Simple function to get current time in milliseconds. Useful for time comparisons
-    # ex.  startTime = ObjectName.GetTimeMS()
-    #      endTime = ObjectName.GetTimeMS()
-    #      totalTimeDifferenceInMilliseconds = (endTime - startTime)
-    def GetTimeMS(self,):
-        return round(time.time() * 1000)
+from datetime import datetime
+from pycomm3 import LogixDriver
+from pycomm3.exceptions import CommError
+from influxdb import InfluxDBClient
 
 
-    # Function to check for connection status to Thingworx platform based upon time interval.
-    def ConnectionStatus(self):
-        timerPreset = self.ConnectionStatusTime * 1000
-        if (((self.GetTimeMS() - self._LastStatusUpdate) >= timerPreset) and not self._ConnectionStatusRunning):
-            self._ConnectionStatusRunning = True
-            self._LastStatusUpdate = self.GetTimeMS()
-            threading.Thread(target=self._ConnectionStatus).start()
+def main():
 
-    #Run REST request against EMS to see if it is connected to the Thingworx platform
-    def _ConnectionStatus(self,):
-        url = 'http://localhost:8000/Thingworx/Things/LocalEms/Properties/isConnected'
+    # Setup Influx Connection
+    client = InfluxDBClient('localhost', 8086, 'root', '$ani-Matic2855', 'sanitrend')
+    client.switch_database('sanitrend')
+
+    # Set up SaniTrend parameters, tags, cloud configurations, etc...
+    SaniTrend = SaniTrendCloud.Config(ConfigFile="../SaniTrendConfig.json")
+
+    # Setup PLC Communication Driver
+    PLC = LogixDriver(SaniTrend.PLCIPAddress)
+
+    PLCErrorCount = 0
+    runCode = True
+
+    while runCode:
         try:
-            serviceResult = self._ConnectionStatusSession.get(url, headers=self._HttpHeaders, timeout=5)
-            if serviceResult.status_code == 200:
-                self.isConnected = (serviceResult.json())['rows'][0]['isConnected']
-                
+            # Get Connection Status For EMS
+            SaniTrend.ConnectionStatus()
+
+            # Database Paylod
+            json_payload = []
+
+            if PLC.connected:
+                # Read PLC tags
+                tagData = PLC.read(*SaniTrend.Tags)
+                inches = SaniTrend.GetTagValue(TagData=tagData, TagName='Inches')
+                volts = SaniTrend.GetTagValue(TagData=tagData, TagName='Volts')
+                print(f'Connection Status = {SaniTrend.isConnected} inches value = {inches}')
+
+                data = {
+                         "measurement" : "data",
+                         "tags": {},
+                         "time" :datetime.now(),
+                         "fields" : {
+                             "SentToTwx" : False,
+                             "Inches" : inches,
+                             "Volts" : volts
+                             }
+                         }
+                json_payload.append(data)
+
+                # Send Data to InFlux DB
+                client.write_points(json_payload)
+
+                # Reset PLC error count if everything succeeded
+                PLCErrorCount = 0
+
             else:
-                self.LogErrorToFile('_ConnectionStatus', serviceResult)
-                self.isConnected = False
+                PLC.open()
 
-        except Exception as e:
-            self.isConnected = False
-            self.LogErrorToFile('_ConnectionStatus', e)
-            
-        # Release Bit so watchdog can run again
-        self._ConnectionStatusRunning = False
+            result = client.query('select * from data where SentToTwx = false')
+            results = list(result)
+            print(results)
+            time.sleep(5)
+            json_payload2 = []
 
+            for row in results:
+                for dict in row:
+                    data = {
+                            "measurement" : "data",
+                            "tags" : {},
+                            "time" : 0,
+                            "fields" : {}
+                            }
 
-    def LogErrorToFile(self, name, error):
-        errorTopDirectory = f'../ErrorLogs'
-        currentDateTime = datetime.now()
-        errorYear = str(currentDateTime.year)
-        errorYearDirectory  = os.path.join(errorTopDirectory, errorYear)
-        errorMonth = currentDateTime.strftime('%B')
-        errorMonthDirectory = os.path.join(errorYearDirectory, errorMonth)
-        directories = [errorTopDirectory, errorYearDirectory, errorMonthDirectory]
-        # Try to create directories, if they exists move on
-        for directory in directories:
-            try:
-                os.mkdir(directory)
-            except: 
-                pass
+                    fields = {}
+                    for key,value in dict.items():
+                        if key != 'time' and key != 'SentToTwx':
+                            fields[key] = value
+                        elif key == 'time':
+                            data[key] = value
+                        elif key == 'SentToTwx':
+                            fields[key] = True
+                    data['fields'] = fields
+                    json_payload2.append(data)
 
-        day = currentDateTime.day if currentDateTime.day < 10 else f'0{currentDateTime.day}'
-        month = currentDateTime.month if currentDateTime.month < 10 else f'0{currentDateTime.month}'
-        errorLog = f'STC_Errors_{datetime.now().year}{month}{day}.log'
-        writePath = os.path.join(errorMonthDirectory, errorLog)
-        mode = 'a+' if os.path.exists(writePath) else 'w+'
-        with open(writePath, mode) as f:
-            f.write(f'{currentDateTime},{name},{error}\n')
-        
-        print(f'{currentDateTime},{name},{error}\n')
+            client.write_points(json_payload2)
+            print(json_payload2)
+
+        except CommError:
+            PLCErrorCount += 1
+            print(f'Communication Error! Fail Count: {PLCErrorCount}')
+            SaniTrend.LogErrorToFile('PLC Comms Failed', CommError)
+            if PLCErrorCount < 6:
+                time.sleep(10)
+            else:
+                time.sleep(30)
+            PLC = LogixDriver(SaniTrend.PLCIPAddress)
+
+        except KeyboardInterrupt:
+            print("\n\nExiting Python and closing PLC connection...\n\n\n")
+            PLC.close()
+            runCode = False
+
+        except Exception as error:
+            print(error)
+            print('Shutting Down...')
+            PLC.close()
+            runCode = False
+
+if __name__ == "__main__":
+    main()
