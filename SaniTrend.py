3f3bff4bdee243dc0519b616361c300b6db60dd6
msienkow <msienkow@gmail.com>
2021-12-09 15:16:44 -0600

    test

--- SaniTrend.py
+++ SaniTrend.py
@@ -148,10 +148,9 @@ def main():
             disp.show()
             currentSeconds = time.perf_counter()
             if (currentSeconds - lastDisplayUpdate) > disp_time:
+                text_item += 1
                 if text_item >= text_items:
                     text_item = 0
-                else:
-                    text_item += 1
             # text_item += 1
             # if text_item == text_items:
             #     text_item = 0
