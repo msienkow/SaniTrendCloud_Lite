cf0686dd3d87010d049029de936787b7200d6bf8
msienkow <msienkow@gmail.com>
2021-12-09 15:00:30 -0600

    test

--- SaniTrend.py
+++ SaniTrend.py
@@ -134,8 +134,8 @@ def main():
             # text[2]['text'] = connection_text
             draw.rectangle((0, 0, width, height), outline=0, fill=0)
             
-            draw.text((x, top + 0), text[text_item]['title'], font=font, fill=255)
-            draw.text((x, top + 16), text[text_item['text']], font=font, fill=255)
+            draw.text((x, top + 0), text[text_item].title, font=font, fill=255)
+            draw.text((x, top + 16), text[text_item].text, font=font, fill=255)
             # draw.text((x, top + 8), voltage_text, font=font, fill=255)
             # draw.text((x, top + 16), value_text, font=font, fill=255)
             # draw.text((x, top + 25), connection_text, font=font, fill=255)
