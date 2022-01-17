9262905052edaabf751befeff31a60d382eba342
msienkow <msienkow@gmail.com>
2022-01-17 17:13:52 -0600

    test

--- twxlog.py
+++ twxlog.py
@@ -25,7 +25,7 @@ with open("TwxData.log", "r+") as file:
         print(result)
         if result == 200:
             file.seek(0)
-            for i in num_lines:
+            for _ in range(num_lines):
                 file.write("")
             file.truncate()
     else:
