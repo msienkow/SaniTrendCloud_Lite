d91ac3c182382d633288f2a23be083625ece881d
msienkow <msienkow@gmail.com>
2022-01-19 12:19:15 -0600

    test

--- twxlog.py
+++ twxlog.py
@@ -1,3 +1,4 @@
+from email import message
 import SaniTrendCloud
 import ast
 import time
@@ -9,29 +10,24 @@ num_lines = 0
 
 
 
-with open("TwxData.log", "r+") as file:
-    clear_file = True
-    count = 0
+with open("TwxData.log", "w+") as file:
+    lines = file.readlines()
     file.seek(0)
-    for _ in file:
+    file.truncate()
+    for line in lines:
         twx_data = []
-        line = file.readline().strip()
-        if line != "pass":
-            data = ast.literal_eval(line)
-            
-            for item in data:
-                twx_data.append(item)
-            
-            result = SaniTrend._LogThingworxData(twx_data)
-            if result == 200:
-                file.write("pass")
-                count = count + 1
-                print(result)
+        message = line.strip()
+        data = ast.literal_eval(message)
+        
+        for item in data:
+            twx_data.append(item)
+        
+        result = SaniTrend._LogThingworxData(twx_data)
 
-            elif result != 200 and clear_file:
-                clear_file = False
-                print("failed")
-    print(count)
-    if clear_file:
-        # file.truncate(0)
-        pass
+        if result == 200:
+            count = count + 1
+            print(result)
+
+        elif result != 200:
+            file.write(line)
+            print("failed")
