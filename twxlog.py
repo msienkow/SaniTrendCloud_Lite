3f9d90974984045e2fdbc539865eb325a51848dd
msienkow <msienkow@gmail.com>
2022-02-03 16:21:32 -0600

    sqlite3 testing

--- twxlog.py
+++ twxlog.py
@@ -1,13 +1,34 @@
-import SaniTrendCloud
 import sqlite3
+import os
+import time
 
-db = sqlite3.connect('STC.db')
-cur = db.cursor()
 
-cur.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='sanitrend' ''')
-if cur.fetchone()[0] == 0:
-    cur.execute(''' CREATE TABLE sanitrend (data text, twx integer) ''')
-else:
-    print('exits')
-db.commit()
-db.close
+DEFAULT_PATH = os.path.join(os.path.dirname(__file__), 'STC.db')
+start = time.perf_counter()
+with sqlite3.connect(database=DEFAULT_PATH) as db:
+    cur = db.cursor()
+    insert_query = ''' INSERT INTO sanitrend (data, twx) VALUES (?,?); '''
+    records = []
+
+
+    # cur.execute(''' CREATE TABLE if not exists sanitrend (data text, twx integer) ''')
+    # for i in range(1000000):
+    #     text = f'Test {i + 1}'
+    #     records.append((text, False))
+
+    # cur.executemany(insert_query, records)
+    # db.commit()
+
+    query = '''select ROWID,data,twx from sanitrend where twx = false'''
+    cur.execute(query)
+    records2 = cur.fetchall()
+    sql = ''' DELETE FROM sanitrend where ROWID=? '''
+    for row in records2:
+        print(row)
+        id = row[0];
+        cur.execute(sql, (id,))
+    db.commit()
+
+end = time.perf_counter()
+totaltime = end - start
+print(f'Total Time: {totaltime} seconds')
