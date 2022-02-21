9c980e6c89648c616917e81f082af92a8f693530
msienkow <msienkow@gmail.com>
2022-02-21 13:44:17 -0600

    test

--- SaniTrend.py
+++ SaniTrend.py
@@ -6,52 +6,53 @@ from pycomm3.exceptions import CommError
 def main():
     # Set up SaniTrend parameters, tags, cloud configurations, etc...
     SaniTrend = SaniTrendCloud.SaniTrend(ConfigFile="../SaniTrendConfig.json")
-
-    # Setup PLC Communication Driver
-    PLC = LogixDriver(SaniTrend.PLCIPAddress)
+    
     PLCErrorCount = 0
     
     runCode = True
 
     while runCode:
         try:
-            # Get Connection Status For EMS
-            SaniTrend.ConnectionStatus()
-
-            if PLC.connected:
-                PLCErrorCount = 0
-                scan_plc = SaniTrend.PLCScanTimerDN()               
-                if scan_plc:
-                    # Read PLC tags
-                    SaniTrend.TagData = PLC.read(*SaniTrend.Tags)
+            with LogixDriver(SaniTrend.PLCIPAddress) as PLC:
+                # Get Connection Status For EMS
+                SaniTrend.ConnectionStatus()
+            
+                # Send in-memory data to Thingworx
+                if len(SaniTrend.TwxDataRows) > 0:
+                    SaniTrend.SendDataToTwx()
                     
-                    # Store Data to in-memory Log
-                    SaniTrend.LogData()
-
-                    # Update SaniTrend Watchdog Bit
-                    PLC.write('SaniTrend_Watchdog', SaniTrend.GetTagValue(TagName='PLC_Watchdog'))
-
-                # Update parameters in PLC from Thingworx values in cloud
-                SaniTrend.GetVirtualSetupData()
-                if not SaniTrend.ConfigUpdateRunning and SaniTrend.isConnected and SaniTrend.Virtual_Tag_Config:
-                    PLC.write(*SaniTrend.Virtual_Tag_Config)
+                connected = PLC.connected
                 
-                # Check if pc has reboot request
-                reboot = SaniTrend.GetTagValue(TagName='Reboot')
-                if reboot:
-                    PLC.write('Reboot_Response', 2)
-                    runCode = False
-                    PLC.close()
-                    if not SaniTrend.Logging:
-                        time.sleep(2)
-                        SaniTrend.RebootPC()
+                # Code to be run only while a connection to the PLC exists
+                while connected:
+                    PLCErrorCount = 0
+                    scan_plc = SaniTrend.PLCScanTimerDN()               
+                    if scan_plc:
+                        # Read PLC tags
+                        SaniTrend.TagData = PLC.read(*SaniTrend.Tags)
+                        
+                        # Store Data to in-memory Log
+                        SaniTrend.LogData()
 
-            else:
-                PLC.open()
+                        # Update SaniTrend Watchdog Bit
+                        PLC.write('SaniTrend_Watchdog', SaniTrend.GetTagValue(TagName='PLC_Watchdog'))
 
-            # Send in-memory data to Thingworx
-            if len(SaniTrend.TwxDataRows) > 0:
-                SaniTrend.SendDataToTwx()
+                    # Update parameters in PLC from Thingworx values in cloud
+                    SaniTrend.GetVirtualSetupData()
+                    if not SaniTrend.ConfigUpdateRunning and SaniTrend.isConnected and SaniTrend.Virtual_Tag_Config:
+                        PLC.write(*SaniTrend.Virtual_Tag_Config)
+                    
+                    # Check if pc has reboot request
+                    reboot = SaniTrend.GetTagValue(TagName='Reboot')
+                    if reboot:
+                        PLC.write('Reboot_Response', 2)
+                        runCode = False
+                        PLC.close()
+                        if not SaniTrend.Logging:
+                            time.sleep(2)
+                            SaniTrend.RebootPC()
+
+            
                    
         except CommError:
             PLCErrorCount += 1
@@ -61,18 +62,16 @@ def main():
                 time.sleep(10)
             else:
                 time.sleep(30)
-            PLC = LogixDriver(SaniTrend.PLCIPAddress)
 
         except KeyboardInterrupt:
             print("\n\nExiting Python and closing PLC connection...\n\n\n")
-            PLC.close()
             runCode = False
 
         except Exception as error:
             print(error)
-            print('Shutting Down...')
-            PLC.close()
+            print('Final Exception. Restarting Code in 30 seconds...')
             SaniTrend.LogErrorToFile('Final Exception', error)
+            time.sleep(30)
             # runCode = False
 
 if __name__ == "__main__":
